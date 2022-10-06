a00ffa63f005665200793e3188f32d0559b89955
RunyShark <spartadragon117@live.com.mx>
2022-10-06 17:33:54 -0500

    StyleSheet

--- src/screens/Counter/index.tsx
+++ src/screens/Counter/index.tsx
@@ -1,30 +1,41 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 
 export const CounterScreen = () => {
   const [counter, setCounter] = useState<number>(0);
 
-  const hadleTouch = (n: number) => {
-    setCounter(counter + n);
-  };
-
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text
-        style={{
-          textAlign: 'center',
-          fontSize: 40,
-          top: -40,
-          color: 'black',
-        }}>
-        Counter: {counter}
-      </Text>
-      <Button onPress={() => hadleTouch(1)} title="+" />
-
-      <Button onPress={() => hadleTouch(-1)} title="-" />
-
-      <Button onPress={() => setCounter(0)} title="reset" />
+    <View style={styles.container}>
+      <Text style={styles.title}>Counter: {counter}</Text>
+      <TouchableOpacity onPress={() => setCounter(counter + 1)}>
+        <Text>1+</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setCounter(counter - 1)}>
+        <Text>1-</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setCounter(0)}>
+        <Text>restart</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#eaeaea',
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
