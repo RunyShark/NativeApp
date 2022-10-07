3c6f57d230b2cca151f4b7fb8e805ea3f4be8546
RunyShark <spartadragon117@live.com.mx>
2022-10-06 21:40:24 -0500

    width

--- src/screens/Dimensione/index.tsx
+++ src/screens/Dimensione/index.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  // Dimensions,
+  Text,
+  useWindowDimensions,
+} from 'react-native';
 
 export const DimensionScreen = () => {
+  // const {height, width} = Dimensions.get('window'); // no change rotation
+  const {height, width} = useWindowDimensions(); // change rotation
   return (
     <View>
-      <Text>Hello word</Text>
+      <View style={styles.container}>
+        <View style={{...styles.boxPurple, width: width * 0.5}} />
+        <View style={styles.boxOrange} />
+      </View>
+      <Text style={styles.title}>
+        W: {width} H: {height}
+      </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 600,
+    backgroundColor: 'red',
+  },
+  boxPurple: {
+    backgroundColor: '#5856D6',
+    width: '50%',
+    height: '50%',
+  },
+  boxOrange: {
+    backgroundColor: '#F0A23B',
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
